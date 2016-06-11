dad01d52dd81d902ca3af2ecd417f787a9e741b1
Aalaa Mohamed <alaa.abdulmawla1993@gmail.com>
2016-06-11 14:57:12 +0300

    Creating Users Models and Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, 
+    							:phone_num, :email, :password, :password_confirmation,
+    						    :gender, :birthdate, :hometown, :about, :marital_status,
+    							:avatar]
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, 
+    							:phone_num, :email, :password_confirmation,
+    						    :gender, :birthdate, :hometown, :about, :marital_status,
+    							:avatar_cache, :remove_avatar, :avatar]
+    devise_parameter_sanitizer.for(:sign_in) << [:email, :remember_me]
+  end
+
+ 
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  mount_uploader :avatar, ImageUploader
+
+  #validations
+  validates :first_name,:last_name, :email, :gender,  presence: true
+  validates :first_name,:last_name, :email, :hometown,  length: { maximum: 100 }
+  validates :about , length: {maximum: 500}
+  validates :avatar_url, allow_blank: true, format:{
+ 	with: %r{\.(gif|jpg|png)\Z}i,
+ 	message: 'must be a URL for GIF, JPG, PNG image.'
+  }
+
+  validates :phone_num, allow_blank:true, numericality: { only_integer: true },
+                 :length => { :maximum => 15 }
+
+
+
+end
