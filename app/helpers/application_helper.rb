159b075eec9ba2361116283070f8828873836973
Aalaa Mohamed <alaa.abdulmawla1993@gmail.com>
2016-06-18 02:35:03 +0300

    ay 7aga

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def default_img(user)
+	  if user.avatar_url.nil?
+	  	if (user.gender = "Male")
+	   		 "Male.PNG"
+	    else
+	    	"female.PNG"
+	    end
+	  else
+	    user.avatar_url
+	  end
+	end
+
+	def full_name(user)
+		user.first_name + " " + user.last_name
+	end
 end
