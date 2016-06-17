9e689038b10fa061b0c77eb08b7bad45b2ed45db
Aalaa Mohamed <alaa.abdulmawla1993@gmail.com>
2016-06-17 19:41:39 +0300

    Initial Profile/Home CSS

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,15 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def self.find_posts(user)
-  	posts = user.posts.all + Post.find_by_sql("SELECT * FROM posts where is_public = 'Public'") + 
-  	Post.find_by_sql("SELECT * FROM posts where user_id IN (
-  		 SELECT id FROM users where id IN ( SELECT to_id FROM friendables WHERE friendables.from_id = #{user.id} AND accepted = 1
+  	Post.find_by_sql("SELECT * FROM posts where is_public = 'Public'
+                     UNION
+                     SELECT * FROM posts where user_id = '#{user.id}'
+                     UNION
+                     SELECT * FROM posts where user_id IN (
+  		                  SELECT id FROM users where id IN ( SELECT to_id FROM friendables WHERE friendables.from_id = #{user.id} AND accepted = 1
   		 									UNION
-  		 									SELECT from_id FROM friendables WHERE friendables.to_id = #{user.id} AND accepted = 1) )")
-  	posts.uniq
+  		 									SELECT from_id FROM friendables WHERE friendables.to_id = #{user.id} AND accepted = 1) ) 
+                     ORDER BY created_at DESC")
   end
 
   mount_uploader :attachment, AvatarUploader
